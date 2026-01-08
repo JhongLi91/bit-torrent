1e3a9fe55e6df3e3b99ccc7c1fbb762b61a4f2f8
Jian Hong Li <lijh0901@gmail.com>
2026-01-08 13:16:40 -0500

    feat: job and progress class header

---
+++ include/download/job.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "parsing/buffer.h"
+#include <cstdint>
+#include <queue>
+
+const uint32_t MAX_JOBS_IN_QUEUE = 5;
+
+struct job {
+    job();
+    job(uint32_t idx, uint32_t block_len, uint32_t piece_len);
+    void append_data(buffer_t &buf);
+    bool is_finished();
+
+    bool working;
+
+    uint32_t idx;
+    uint32_t begin;
+    uint32_t block_len;
+    uint32_t piece_len;
+
+    std::queue<uint32_t> pending;
+    buffer_t data;
+};
---
+++ include/download/progress.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "parsing/buffer.h"
+#include <cstdint>
+#include <vector>
+
+class progress {
+
+  public:
+    progress(uint32_t n);
+    bool is_complete();
+    int next_piece_to_request(buffer_t &peer_bitfield);
+    void update_have(uint32_t piece);
+    void update_receive(uint32_t piece);
+
+  public:
+    uint32_t total;
+    uint32_t num_received;
+
+    buffer_t pending;
+    buffer_t received;
+    std::vector<uint32_t> have;
+};
